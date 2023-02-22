e99330021295f8958860e65fef6988479598ac8a
wahlinski <125895164+wahlinski@users.noreply.github.com>
2023-02-22 15:32:20 +0100

    Delete Driver.java

--- Driver.java
+++
@@ -1,9 +0,0 @@
-package calculator;
-public class Driver{
-    public static void main(String[] args){
-        //
-        //  Inizializes the calculator as a GUI instance on program start.
-        //
-        new GUI();
-    }
-}
