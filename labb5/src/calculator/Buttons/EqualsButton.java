45868ce2434a0cab357abcf16490c22c7347d378
wahlinski <125895164+wahlinski@users.noreply.github.com>
2023-02-27 14:43:49 +0100

    Finalized version of Equals button

--- labb5/src/calculator/Buttons/EqualsButton.java
+++ labb5/src/calculator/Buttons/EqualsButton.java
@@ -2,33 +2,42 @@ package calculator.Buttons;
 
 import calculator.Situation;
 
+import static calculator.State.*;
+
 public class EqualsButton extends CalculatorButton{
-    private String label;
-    private Situation situation;
     public EqualsButton(String label, Situation situation){
+        //
         super(label, situation);
-        this.label = label;
-        this.situation = situation;
     }
+    // Override the transition method from the CalculatorButton class, now that we made it concrete method.
     @Override
     public void transition() {
-        switch (situation.getState()){
+        switch (situation.getState()){   //switch based on current state
             case Input1:
-                situation.setLeftOperand(0);
-                situation.getDisplay().setText("0");
+                // If the calculator is in the Input1 state, set the display to show the left operand
+                situation.setState(Input1);
+                situation.getDisplay().setText(Integer.toString(situation.getLeftOperand()));
                 break;
             case OpReady:
-                situation.setLeftOperand(0);
-                situation.getDisplay().setText("0");
+                // If the calculator is in the OpReady state, set the display to show the left operand
+                situation.setState(OpReady);
+                situation.getDisplay().setText(Integer.toString(situation.getLeftOperand()));
                 break;
             case Input2:
-                situation.setLeftOperand(0);
-                situation.getDisplay().setText("0");
+                // If the calculator is in the Input2 state, perform the appropriate math operation on the left and right operands,
+                // set the result as the new left operand, and update the state of the calculator
+                int result= situation.getBinaryOperatorButton().getBinaryOperator().applyAsInt(situation.getLeftOperand(), Integer.parseInt(situation.getDisplay().getText()) );
+                situation.getDisplay().setText(Integer.toString(result));
+                situation.setLeftOperand(result);
+                situation.setState(HasResult);
+                situation.getBinaryOperatorButton().resetButton();
                 break;
             case HasResult:
-                situation.setLeftOperand(0);
-                situation.getDisplay().setText("0");
+                //set the display to show the left operand
+                situation.setState(HasResult);
+                situation.getDisplay().setText(Integer.toString(situation.getLeftOperand()));
                 break;
         }
     }
 }
+
